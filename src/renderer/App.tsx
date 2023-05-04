b9bde61a43dfa4b0de759476ddb8c84a1d0e8cb5
iancleary <github@iancleary.me>
2023-05-03 20:56:23 -0700

    Update to use Styled Component

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,40 +1,31 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import icon from '../../assets/icon.svg';
 import './App.css';
+
+const Button = styled.button`
+  padding: 10px 20px;
+  border-radius: 10px;
+  border: none;
+  appearance: none;
+  font-size: 1.3rem;
+  box-shadow: 0px 8px 28px -6px rgba(24, 39, 75, 0.12),
+    0px 18px 88px -4px rgba(24, 39, 75, 0.14);
+  transition: all ease-in 0.1s;
+  cursor: pointer;
+  opacity: 0.9;
+`;
+
+// const SideBar = styled.div``;
 
 function Hello() {
   return (
     <div>
       <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
+        <img width="150" alt="icon" src={icon} />
       </div>
-      <h1>Horizon</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+      <h1 className="Title">Horizon</h1>
+      <Button>Hello, I&apos;m a button</Button>
     </div>
   );
 }
