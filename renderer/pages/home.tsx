9d03bae38a11cf5af27b9d62de44de803e6909d9
iancleary <github@iancleary.me>
2023-05-05 21:02:52 -0700

    Add styled components

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -2,6 +2,26 @@ import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 
+import styled from 'styled-components';
+
+const Button = styled.button`
+  padding: 10px 20px;
+  border-radius: 10px;
+  border: none;
+  appearance: none;
+  font-size: 1.3rem;
+  box-shadow: 0px 8px 28px -6px rgba(24, 39, 75, 0.12),
+    0px 18px 88px -4px rgba(24, 39, 75, 0.14);
+  transition: all ease-in 0.1s;
+  cursor: pointer;
+  opacity: 0.9;
+
+  :hover {
+    transform: scale(1.05);
+    opacity: 1;
+  }
+`;
+
 function Home() {
   return (
     <React.Fragment>
@@ -14,6 +34,7 @@ function Home() {
           <Link href="/next">
             <a>Go to next page</a>
           </Link>
+          <Button>Styled Button</Button>
         </p>
         <img src="/images/logo.png" />
       </div>
