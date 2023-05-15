caabb729180d5bf536ffc6ae542aa7e216985541
iancleary <github@iancleary.me>
2023-05-14 17:14:04 -0700

    Make StyledComponentRegistry a jsx file

--- renderer/components/StyledComponentsRegistry/StyledComponentRegistry.jsx
+++ renderer/components/StyledComponentsRegistry/StyledComponentRegistry.jsx
@@ -16,8 +16,6 @@ import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
 export default function StyledComponentsRegistry({
   children,
-}: {
-  children: React.ReactNode;
 }) {
   // Only create stylesheet once with lazy initial state
   // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
