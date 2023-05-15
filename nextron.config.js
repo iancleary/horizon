580f941b9fccff992cd0c44202e3b5343367fb2a
iancleary <github@iancleary.me>
2023-05-14 17:32:59 -0700

    Update git, nextron, tsconfig files to use src/ instead of renderer/

--- nextron.config.js
+++ nextron.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // specify an alternate main src directory, defaults to 'main'
   mainSrcDir: 'main',
   // specify an alternate renderer src directory, defaults to 'renderer'
-  rendererSrcDir: 'renderer',
+  rendererSrcDir: 'src',
 
 
   // main process' webpack config
