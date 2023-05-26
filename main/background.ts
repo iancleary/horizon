d2b151e72f5cbdcd90f81a4754b5ab84abd60264
iancleary <github@iancleary.me>
2023-05-25 17:52:22 -0700

    Update to protocol for nextron

--- main/background.ts
+++ main/background.ts
@@ -4,8 +4,11 @@ import { createWindow } from './helpers';
 
 const isProd: boolean = process.env.NODE_ENV === 'production';
 
+const protocol = 'app'; // nextron hardcoded this
+https://github.com/saltyshiomix/nextron/blob/16c5b30bd8931056dc2380f230354ad759abcfa8/lib/nextron-build.ts#L73-L75
+
 if (isProd) {
-  serve({ directory: 'app' });
+  serve({ directory: `${protocol}` });
 } else {
   app.setPath('userData', `${app.getPath('userData')} (development)`);
 }
@@ -17,9 +20,10 @@ if (isProd) {
     width: 1000,
     height: 600,
   });
+
   const initialRoute = 'home'; // without the initial slash
   if (isProd) {
-    await mainWindow.loadURL(`app://./${initialRoute}.html`);
+    await mainWindow.loadURL(`${protocol}://./${initialRoute}.html`);
   } else {
     const port = process.argv[2];
     await mainWindow.loadURL(`http://localhost:${port}/${initialRoute}/`);
--- nextron.config.js
+++ nextron.config.js
@@ -4,6 +4,9 @@ module.exports = {
   // specify an alternate renderer src directory, defaults to 'renderer'
   rendererSrcDir: 'src',
 
+  // appdir: 'app',
+  // This is hardcoded in nextron
+  // https://github.com/saltyshiomix/nextron/blob/16c5b30bd8931056dc2380f230354ad759abcfa8/lib/nextron-build.ts#L73-L84
 
   // main process' webpack config
   webpack: (config, env) => {
