19f7c071d70a75e30ed340c1cd9b155495e69cce
iancleary <github@iancleary.me>
2023-05-25 17:52:08 -0700

    Update Sidebar

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -7,26 +7,13 @@ export const metadata: Metadata = {
   title: 'Home - Nextron (with-typescript)',
 };
 
-async function getData() {
-  const res = await fetch('http://localhost:8000/lists/all');
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data');
-  }
-
-  return res.json();
-}
-
 export default async function DashboardLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
-  const data = await getData();
+  // const data = await getData();
+  const data = [];
   return (
     <>
       {/* Include shared UI here e.g. a header or sidebar */}
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,13 +3,63 @@
 import React from "react";
 import styled from "styled-components";
 
+async function getData() {
+  const res = await fetch('http://127.0.0.1:8000/lists/all');
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+
 const Sidebar = ({children, lists}) => {
+
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+
+
+  React.useEffect(() => {
+    // data fetching here
+    fetch(`http://127.0.0.1:8000/lists/all`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json()
+      })
+      .then((actualData) => {
+        setData(actualData);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData([]);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <Wrapper>
       <div>
         <h1>Lists</h1>
         <ul>
           {lists.map((list: any) => (
+            <li key={list.id}>{list.name}</li>
+          ))}
+        </ul>
+        <ul>
+          {data.map((list: any) => (
             <li key={list.id}>{list.name}</li>
           ))}
         </ul>
